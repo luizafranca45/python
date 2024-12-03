6a224429f5f62313d0b71d0f7bbb186d4e64447a
Luiza <luizaachilles061@gmail.com>
2024-12-03 11:32:55 -0300

    Aula 2 sobre variáveis.

---
+++ Aula 2/Aula02 .py
@@ -0,0 +1,11 @@
+print('Olá pessoal')
+
+nome_usuario = 'Luiza' #tipo -> str = string 
+idade_usuario = 17 # sou linda
+altura_usuario = 1.65 # tipo -> float = ponto flutuante 
+maior_idade = True # tipo -> bool = boolean
+
+print('Seja Bem-vindo', nome_usuario)
+print('sua idade é', idade_usuario,'anos')
+print('voce tem', altura_usuario,"metros.")
+print('voce é maior de idade?', maior_idade)
